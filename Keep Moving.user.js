33b680600c24aff93849ff44708eb9304f5aeeec
Lav-i <lan_xiao163@163.com>
2019-11-29 10:49:57 +0800

    增加后台播放，优化检查方法

--- Keep Moving.user.js
+++ Keep Moving.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Keep Moving
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  keep moving on video
 // @author       Lavi
 // @match        https://grabuct.xuetangx.com/lms*
@@ -16,31 +16,30 @@
 (function () {
     'use strict'
 
-    window.onload = function () {
+    setTimeout(function () {
+
         console.log('keep moving')
 
-        var time = document.getElementsByClassName('xt_video_player_current_time_display fl')[0].children
-        var startTime = time[0].textContent
-        var stopTime = time[1].textContent
+        var video = document.getElementsByTagName('video')[0]
 
+        video.play()
         document.getElementsByClassName("xt_video_player_common_list")[0].firstChild.click()
 
-        function compare() {
-            startTime = time[0].textContent
-            stopTime = time[1].textContent
-
-            if (startTime == stopTime) {
-                document.getElementsByClassName("select-chapter")[0].click()
-                var activateVideo = document.getElementsByClassName("section-video-name video-active")[0]
-                var nextVideo = activateVideo.parentNode.parentNode.parentNode.nextSibling
-                nextVideo.firstChild.firstChild.lastChild.click()
-                setTimeout(function () {
-                    document.getElementsByClassName("element-wrap")[1].click()
-                }, 1000)
-            }
+        video.onpause = function () {
+            video.play()
+            document.getElementsByClassName("xt_video_player_common_list")[0].firstChild.click()
         }
 
-        this.setInterval(compare, 10000)
+        video.onended = function () {
+            document.getElementsByClassName("select-chapter")[0].click()
+            var activateVideo = document.getElementsByClassName("section-video-name video-active")[0]
+            var nextVideo = activateVideo.parentNode.parentNode.parentNode.nextSibling
+            nextVideo.firstChild.firstChild.lastChild.click()
+            setTimeout(function () {
+                document.getElementsByClassName("element-wrap")[1].click()
+            }, 1000)
+        }
 
-    }
+    }, 5000)
+
 })()
